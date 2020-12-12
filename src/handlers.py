badac7b45a94a4fd30a78464c628eaef0aae99a7
masterboda <magisterboda.go.ua@gmail.com>
2020-12-12 11:34:17 +0200

    Fixed send media

--- src/handlers.py
+++ src/handlers.py
@@ -115,19 +115,19 @@ def forward_message(receiver_id, message, context):
     context.bot.send_message(receiver_id, 'Hey! You have new message:')
 
     if message.photo:
-        context.bot.send_photo(chat_id=receiver_id, photo=message.photo, caption=message.caption)
+        context.bot.send_photo(chat_id=receiver_id, photo=message.photo[-1].file_id, caption=message.caption)
     if message.video:
-        context.bot.send_video(chat_id=receiver_id, video=message.video, caption=message.caption)
+        context.bot.send_video(chat_id=receiver_id, video=message.video.file_id, caption=message.caption)
     if message.audio:
-        context.bot.send_audio(chat_id=receiver_id, audio=message.audio, caption=message.caption)
+        context.bot.send_audio(chat_id=receiver_id, audio=message.audio.file_id, caption=message.caption)
     if message.document:
-        context.bot.send_document(chat_id=receiver_id, document=message.document, caption=message.caption)
+        context.bot.send_document(chat_id=receiver_id, document=message.document.file_id, caption=message.caption)
     if message.sticker:
-        context.bot.send_sticker(chat_id=receiver_id, sticker=message.sticker)
+        context.bot.send_sticker(chat_id=receiver_id, sticker=message.sticker.file_id)
     if message.voice:
-        context.bot.send_voice(chat_id=receiver_id, voice=message.voice)
+        context.bot.send_voice(chat_id=receiver_id, voice=message.voice.file_id)
     if message.video_note:
-        context.bot.send_video_note(chat_id=receiver_id, video_note=message.video_note)
+        context.bot.send_video_note(chat_id=receiver_id, video_note=message.video_note.file_id)
     if message.text:
         context.bot.send_message(receiver_id, message.text)
 
@@ -145,12 +145,12 @@ def send(cursor, update: Update, context: CallbackContext):
         }
         cursor.execute('INSERT INTO messages (receiver_id, sender, message) VALUES (?, ?, ?)', (context.user_data['receiver_id'], json.dumps(user), json.dumps(context.user_data['message'].to_dict())))
 
-        try:
-            forward_message(context.user_data['receiver_id'], context.user_data['message'], context)
-            query.edit_message_text(text='Congrats! Message successfully sent!')
-        except Exception:
-            print(Exception)
-            query.edit_message_text(text='Sorry, this user is not accessible yet!')
+        # try:
+        forward_message(context.user_data['receiver_id'], context.user_data['message'], context)
+        query.edit_message_text(text='Congrats! Message successfully sent!')
+        # except Exception:
+        #     print(Exception)
+        #     query.edit_message_text(text='Sorry, this user is not accessible yet!')
 
     elif query.data == markup.CANCEL:
         query.edit_message_text(text='Canceled!')
